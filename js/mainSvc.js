65a1793088a743ff3a721af7baa72027bf3b37bf
Phillip Puckett <phillippuckett88@gmail.com>
2016-05-23 21:25:56 -0600

    Data is plugged into the interface

--- js/mainSvc.js
+++ js/mainSvc.js
@@ -5,9 +5,7 @@ angular.module('zipHubProject')
         this.getYpSvc = function (name, location) {
             return $http({
                 method: 'GET',
-                // url: 'www.google.com'
                 url: 'http://api.sandbox.yellowapi.com/FindBusiness/?what=Restaurants&where=Las+Vegas&pgLen=5&pg=1&dist=1&fmt=JSON&lang=en&UID=0000&apikey=7hremc98vmtm6794zfy67hbv',
-                // headers: { 'X-Originating-Ip': '66.232.90.106' }
             })
                 .then(function success(response) {
                     console.log(response);
