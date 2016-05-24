071f59533538d00f26382fc6a6182ffe994c360c
Phillip Puckett <phillippuckett88@gmail.com>
2016-05-23 21:03:11 -0600

    YASSgit add -A data

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,12 +1,13 @@
 angular.module('zipHubProject')
-    .controller('mainCtrl', function ($scope, mainSvc) {
+    .controller('mainCtrl', function ($scope, $http, mainSvc) {
 
-
+        console.log('CTRL');
         $scope.getYpCtrl = function () {
+            console.log('functioning');
             mainSvc.getYpSvc($scope.name, $scope.location)
-            .then()
+                .then(function (response) {
+                    $scope.data = response;
+                })
         };
-       
-        
 
     });
--- js/mainSvc.js
+++ js/mainSvc.js
@@ -1,14 +1,20 @@
 angular.module('zipHubProject')
     .service('mainSvc', function ($http) {
 
-        this.getYpSvc = function () {
-            return $http.get('api/sexytime')
-                .then(function (response) {
+        console.log('SVC');
+        this.getYpSvc = function (name, location) {
+            return $http({
+                method: 'GET',
+                // url: 'www.google.com'
+                url: 'http://api.sandbox.yellowapi.com/FindBusiness/?what=Restaurants&where=Las+Vegas&pgLen=5&pg=1&dist=1&fmt=JSON&lang=en&UID=0000&apikey=7hremc98vmtm6794zfy67hbv',
+                // headers: { 'X-Originating-Ip': '66.232.90.106' }
+            })
+                .then(function success(response) {
                     console.log(response);
-                    // return response;
+                    return response.data;
+                }, function (err) {
+                    console.log('there was error')
                 })
         };
-        
-        
-        
+
     });
